0bbc0f7d873e5044483740e0189846d81584de43
Aditya5489 <ap5663019@gmail.com>
2025-08-08 22:16:28 +0530

    Updated cors

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    'http://localhost:5173', 
+    'https://prepgenie-frontend.onrender.com' 
+  ],
   credentials: true,
 }));
 
