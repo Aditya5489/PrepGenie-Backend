a2fd95ef7c9389fb9e6e6e67d3d637cdaa064810
Aditya5489 <ap5663019@gmail.com>
2025-08-08 21:22:27 +0530

    Update session middleware with MongoDB store for production

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI,
+    mongoUrl: process.env.MONGO_URL,
     ttl: 60 * 60 * 24 * 7, 
   }),
   cookie: {
